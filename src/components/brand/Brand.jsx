c185d8ad9f0bddece86daab3616457d82c8d3311
Szymon Miękus <miekusoft@gmail.com>
2022-07-18 23:02:20 +0200

    Brand component

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
-import "./brand.css";
+import "./brand.scss";
+import { google, slack, atlasian, shopify, dropbox } from "./imports";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlasian} alt="atlasian" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
