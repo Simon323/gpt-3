84527eae7c0184412e4e41e94d394007f084997f
Szymon Miękus <miekusoft@gmail.com>
2022-07-19 23:10:17 +0200

    Complete possibility section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,31 @@
 import React from "react";
-import "./possibility.css";
+import "./possibility.scss";
+import possibilityImage from "../../assets/possibility.png";
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div className="gpt3__possibility section__padding" id="possibility">
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request early access</h4>
+        <h1 className="gradient__text">
+          Lorem Ipsum is simply dummy text of the printing and
+          typesettingindustry.
+        </h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.
+        </p>
+        <h4>Request early access</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
