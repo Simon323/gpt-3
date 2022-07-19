a371d4ca2a1104b0312c6c3498152e670c26cb7d
Szymon Miękus <miekusoft@gmail.com>
2022-07-19 22:25:45 +0200

    Complete Features section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,8 +1,43 @@
 import React from "react";
-import "./features.css";
+import "./features.scss";
+import { Feature } from "../../components";
+
+const featuresData = [
+  {
+    title: "Lorem Ipsum is simply dummy text",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesettingindustry.",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesettingindustry.",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesettingindustry.",
+  },
+  {
+    title: "Lorem Ipsum is simply dummy text",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesettingindustry.",
+  },
+];
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </h1>
+        <p>Request early access</p>
+      </div>
+      <div className="gpt3__features-container">
+        {featuresData.map(({ text, title }, index) => (
+          <Feature text={text} title={title} key={title + index} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Features;
